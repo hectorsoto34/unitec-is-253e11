2ea030c6ee2e14d81889ed4ff06b35e5da2fa4f7
Alumno49 SUR <Alumno49@myunitecedumx>
2026-03-26 19:22:28 -0600

    Init

---
+++ src/Application262E11/Figura262E11.cs
@@ -0,0 +1,5 @@
+﻿public class Figura262E11
+{
+ public static double Perimetro(double lado) { return lado + lado + lado + lado; }
+ public static double Area(double lado) { return lado * lado; }
+public static double Volumen(double area, double altura) { return area * altura; }
---
+++ src/ConsoleApp262E11/Program.cs
@@ -0,0 +1,8 @@
+﻿double lado = 2;
+double altura = 10;
+double perimetro = Figura262E11.Perimetro(lado);
+double area = Figura262E11.Area(lado);
+double volumen = Figura262E11.Volumen(area, altura);
+Console.WriteLine($"El perímetro de la base de un Paralelogramo de {lado}cm por lado es {perimetro}cm");
+Console.WriteLine($"El área de la base de un Paralelogramo de {lado}cm por lado es {area}cm2");
+Console.WriteLine($"El volumen de un Paralelogramo con un área de la base de {area}cm2 y {altura}cm de altura es {volumen}cm3");
